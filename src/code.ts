a1dc7990d5fcd5baaa0dc8e5fbcfbe538ba86fcb
flavien <flaviendelangle@gmail.com>
2021-09-22 10:16:31 +0100

    Structure typography and palette code

--- src/code.ts
+++ src/code.ts
@@ -1,4 +1,6 @@
-import { decomposeColor } from '@mui/material/styles';
+import {decomposeColor, SimplePaletteColorOptions, ThemeOptions} from '@mui/material/styles';
+
+const isFunction = (func: any): func is Function => typeof func === 'function'
 
 figma.showUI(__html__);
 
@@ -6,20 +8,52 @@ figma.ui.onmessage = (msg) => {
   const { type, payload } = msg;
 
   if (type === 'IMPORT_THEME') {
-    const style = findOrCreateStyle('Primary/Main');
-    const paletteColor = decomposeColor(payload.palette.primary.main);
-    const [red, green, blue] = paletteColor.values;
-    style.paints = [{ type: 'SOLID', color: { r: red / 255, g: green / 255, b: blue / 255 } }];
+    const theme = payload as ThemeOptions
+
+    setPalette(theme.palette)
+    setTypography(theme.typography)
   }
 
   figma.closePlugin();
 };
 
-const findOrCreateStyle = (name: string) => {
+const setPalette = (palette: ThemeOptions['palette'] | undefined) => {
+  if (!palette) {
+    return
+  }
+
+  const style = findOrCreatePaintStyle('Primary/Main');
+  // TODO Handle ColorPartial format
+  const paletteColor = decomposeColor((palette.primary as SimplePaletteColorOptions).main);
+  const [red, green, blue] = paletteColor.values;
+  style.paints = [{ type: 'SOLID', color: { r: red / 255, g: green / 255, b: blue / 255 } }];
+}
+
+const setTypography = (typography: ThemeOptions['typography'] | undefined) => {
+  if (!typography) {
+    return
+  }
+
+  if (isFunction(typography)) {
+    throw new Error('Function typography is not yet supported')
+  }
+}
+
+const findOrCreatePaintStyle = (name: string) => {
   const allStyles = figma.getLocalPaintStyles();
   let style = allStyles.find((style) => style.name === name);
   if (!style) {
     style = figma.createPaintStyle();
+    style.name = name;
+  }
+  return style;
+};
+
+const findOrCreateTextStyle = (name: string) => {
+  const allStyles = figma.getLocalTextStyles();
+  let style = allStyles.find((style) => style.name === name);
+  if (!style) {
+    style = figma.createTextStyle();
     style.name = name;
   }
   return style;
