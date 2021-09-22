0c5b3d7161b254e139aefc820d701c398cd7507d
siriwatknp <siriwatkunaporn@gmail.com>
2021-09-22 18:02:03 +0700

    able to import colors

--- src/code.ts
+++ src/code.ts
@@ -1,6 +1,7 @@
-import {decomposeColor, Theme, SimplePaletteColorOptions, ThemeOptions} from '@mui/material/styles';
+import {decomposeColor, Theme, ThemeOptions} from '@mui/material/styles';
+import {capitalize} from '@mui/material/utils';
 
-const isFunction = (func: any): func is Function => typeof func === 'function'
+const isFunction = (func: any): func is Function => typeof func === 'function';
 
 figma.showUI(__html__);
 
@@ -10,43 +11,45 @@ figma.ui.onmessage = (msg) => {
   if (type === 'IMPORT_THEME') {
     // 1. turn theme into array, so that we can iterate and map the styles in figma
     // ex. { palette: { primary: { main: '#ff5252' }}} => [{ figmaName: 'palette/primary/main', value: '#ff5252' }]
-    const styles = readTheme(payload)
 
     // 2. get localPaintStyles https://www.figma.com/plugin-docs/api/figma/#getlocalpaintstyles
 
     // 3. for each paint style in figma, find the color from (1) and apply the new color
 
     // Do the same for text styles
-    const theme = payload as ThemeOptions
+    const theme = payload as Theme
 
-    setPalette(theme.palette)
-    setTypography(theme.typography)
+    setPalette(theme.palette);
+    setTypography(theme.typography);
   }
 
   // figma.closePlugin();
 };
 
-const setPalette = (palette: ThemeOptions['palette'] | undefined) => {
+const setPalette = (palette: Theme['palette'] | undefined) => {
   if (!palette) {
     return
   }
 
-  const style = findOrCreatePaintStyle('Primary/Main');
-  // TODO Handle ColorPartial format
-  const paletteColor = decomposeColor((palette.primary as SimplePaletteColorOptions).main);
-  const [red, green, blue] = paletteColor.values;
-  style.paints = [{ type: 'SOLID', color: { r: red / 255, g: green / 255, b: blue / 255 } }];
+  const flattenPalettes = readPalette(palette)
+
+  flattenPalettes.forEach(palette => {
+    const style = findOrCreatePaintStyle(palette.figmaName);
+    const paletteColor = decomposeColor(palette.value);
+    const [red, green, blue, opacity = 1] = paletteColor.values;
+    style.paints = [{ type: 'SOLID', color: { r: red / 255, g: green / 255, b: blue / 255 }, opacity }];
+  })
 }
 
 const setTypography = (typography: ThemeOptions['typography'] | undefined) => {
   if (!typography) {
-    return
+    return;
   }
 
   if (isFunction(typography)) {
-    throw new Error('Function typography is not yet supported')
+    throw new Error('Function typography is not yet supported');
   }
-}
+};
 
 const findOrCreatePaintStyle = (name: string) => {
   const allStyles = figma.getLocalPaintStyles();
@@ -69,22 +72,21 @@ const findOrCreateTextStyle = (name: string) => {
 };
 
 
-const readTheme = (theme: Theme) => {
-  const result: Array<{ figmaName: string; value: string | number }> = [];
+const readPalette = (palette: Theme['palette']) => {
+  const result: Array<{ figmaName: string; value: string }> = [];
 
   function iterateDeepObject(object, parentKeys = []) {
     Object.entries(object).forEach(([key, value]) => {
-      if (typeof value === 'string' || typeof value === 'number') {
-        result.push({ figmaName: [...parentKeys, key].join('/'), value })
+      if (typeof value === 'string') {
+        result.push({ figmaName: [...parentKeys, capitalize(key)].join('/'), value })
       }
       if (typeof value !== null && typeof value === 'object') {
-        iterateDeepObject(value, [...parentKeys, key])
+        iterateDeepObject(value, [...parentKeys, capitalize(key)])
       }
     });
   }
 
-  iterateDeepObject(theme);
+  iterateDeepObject(palette);
 
   return result;
 };
-
