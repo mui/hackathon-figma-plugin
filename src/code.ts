5eb40d1b7b061420177fefc7b8deebee9e03260d
siriwatknp <siriwatkunaporn@gmail.com>
2021-09-23 11:48:25 +0700

    update all palettes

--- src/code.ts
+++ src/code.ts
@@ -1,6 +1,5 @@
 import { Theme } from '@mui/material/styles';
-import { capitalize } from '@mui/material/utils';
-import { importPaletteColor } from './import';
+import { importPalette } from './palette';
 import { setTypography } from './typography';
 
 figma.showUI(__html__);
@@ -9,24 +8,11 @@ figma.ui.onmessage = async (msg) => {
   const { type, payload } = msg;
 
   if (type === 'IMPORT_THEME') {
-    // 1. turn theme into array, so that we can iterate and map the styles in figma
-    // ex. { palette: { primary: { main: '#ff5252' }}} => [{ figmaName: 'palette/primary/main', value: '#ff5252' }]
-
-    // 2. get localPaintStyles https://www.figma.com/plugin-docs/api/figma/#getlocalpaintstyles
-
-    // 3. for each paint style in figma, find the color from (1) and apply the new color
-
-    // Do the same for text styles
     const theme = payload as Theme;
 
     const promises = [setTypography(theme.typography)];
 
-    Object.entries(theme.palette).forEach(([key, value]) => {
-      // TODO add user defined type guard
-      if (typeof value === 'object' && value.main) {
-        importPaletteColor(key, value, theme);
-      }
-    });
+    importPalette(theme.palette);
 
     await Promise.all(promises);
 
@@ -34,23 +20,4 @@ figma.ui.onmessage = async (msg) => {
   }
 
   // figma.closePlugin();
-};
-
-const readPalette = (palette: Theme['palette']) => {
-  const result: Array<{ figmaName: string; value: string }> = [];
-
-  function iterateDeepObject(object, parentKeys = []) {
-    Object.entries(object).forEach(([key, value]) => {
-      if (typeof value === 'string') {
-        result.push({ figmaName: [...parentKeys, capitalize(key)].join('/'), value });
-      }
-      if (typeof value !== null && typeof value === 'object') {
-        iterateDeepObject(value, [...parentKeys, capitalize(key)]);
-      }
-    });
-  }
-
-  iterateDeepObject(palette);
-
-  return result;
 };
---
+++ src/palette.ts
@@ -0,0 +1,65 @@
+import { decomposeColor, Theme, PaletteColor, alpha, createTheme } from '@mui/material/styles';
+import { capitalize } from '@mui/material/utils';
+
+const PALETTES = ['primary', 'secondary', 'error', 'warning', 'info', 'success']
+const SUPPORTED_KEYS = [...PALETTES, 'common', 'grey' ,'text', 'divider', 'background', 'action']
+
+const findOrCreatePaintStyle = (name: string) => {
+  const allStyles = figma.getLocalPaintStyles();
+  let style = allStyles.find((style) => style.name === name);
+  if (!style) {
+    style = figma.createPaintStyle();
+    style.name = name;
+  }
+  return style;
+};
+
+const createPaintStyleWithColor = (names: string[], color: string) => {
+  const style = findOrCreatePaintStyle(names.join('/'));
+  const { values: colorRgb } = decomposeColor(color);
+  const [red, green, blue, opacity = 1] = colorRgb;
+  style.paints = [
+    { type: 'SOLID', color: { r: red / 255, g: green / 255, b: blue / 255 }, opacity },
+  ];
+};
+
+
+export const importPalette = (importedPalette: Theme['palette']) => {
+  const resolvedTheme = createTheme({ palette: importedPalette })
+  Object.entries(importedPalette).forEach(([name, value]) => {
+    if (SUPPORTED_KEYS.indexOf(name) !== -1) {
+
+      if (typeof value === 'string') {
+        // eg. divider
+        createPaintStyleWithColor(
+          [capitalize(name)],
+          value
+        );
+      } else {
+        Object.entries(value).forEach(([field, color]: [string, string]) => {
+          const colorName = field === 'contrastText' ? 'contrast' : field;
+          createPaintStyleWithColor([capitalize(name), capitalize(colorName)], color);
+        });
+
+        if (PALETTES.indexOf(name) !== -1) {
+          createPaintStyleWithColor(
+            [capitalize(name), 'States', 'Outlined Resting Border'],
+            alpha(value.main, 0.5),
+          );
+        
+          createPaintStyleWithColor(
+            [capitalize(name), 'States', 'Contained Hover Background'],
+            value.dark,
+          );
+        
+          createPaintStyleWithColor(
+            [capitalize(name), 'States', 'Outlined Hover Background'],
+            alpha(value.main, resolvedTheme.palette.action.hoverOpacity),
+          );
+        }
+      }
+
+      // TODO create styles for Alert/Content and Alert/Background
+    }
+  })
+}
